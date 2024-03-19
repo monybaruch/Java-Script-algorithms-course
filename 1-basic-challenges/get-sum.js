1b68dc975ad285cf6f6140ad1554b470e176fcc0
מוני ברוך <monybaruch@gmail.com>
2024-03-19 09:28:15 +0200

    adding the first basic chllenge

---
+++ 1-basic-challenges/get-sum.js
@@ -0,0 +1,20 @@
+// # Challenge: Get Sum
+
+// This is another very basic practice challenge just to get you into the hang of things.
+
+// ## Instructions
+
+// Write a function called `getSum` that takes in two numbers and returns the sum of those two numbers.
+
+/**
+ * Returns the sum of two numbers.
+ * @param {number} a - The first number.
+ * @param {number} b - The second number.
+ * @returns {number} - The sum of the two numbers.
+ */
+
+const getSum = (a, b) => {
+  return a + b;
+};
+
+console.log(getSum(20, 50));
